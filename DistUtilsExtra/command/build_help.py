478109a03794b9e177c3ae30aca5c02ffc4d5b77
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2014-09-01 16:52:35 +0900

    git-debimport python-distutils-extra_2.38-1.debian.tar.gz

--- DistUtilsExtra/command/build_help.py
+++ DistUtilsExtra/command/build_help.py
@@ -5,7 +5,7 @@ import os.path
 import distutils.cmd
 
 class build_help(distutils.cmd.Command):
-    description = 'install docbook XML based documentation'
+    description = 'install Mallard or DocBook XML based documentation'
     user_options= [('help-dir', None, 'help directory in the source tree')]
 	
     def initialize_options(self):
@@ -18,21 +18,17 @@ class build_help(distutils.cmd.Command):
     def get_data_files(self):
         data_files = []
         name = self.distribution.metadata.name
-        omf_pattern = os.path.join(self.help_dir, '*', '*.omf')
 
         for path in glob(os.path.join(self.help_dir, '*')):
             lang = os.path.basename(path)
-            path_xml = os.path.join('share/gnome/help', name, lang)
-            path_figures = os.path.join('share/gnome/help', name, lang, 'figures')
+            path_xml = os.path.join('share/help', lang, name)
+            path_figures = os.path.join('share/help', lang, name, 'figures')
             
-            docbook_files = glob('%s/*.xml' % path)
+            docbook_files = glob('%s/index.docbook' % path)
+            docbook_files_extra = glob('%s/*.xml' % path)
             mallard_files = glob('%s/*.page' % path)
-            data_files.append((path_xml, docbook_files + mallard_files))
+            data_files.append((path_xml, docbook_files + docbook_files_extra + mallard_files))
             data_files.append((path_figures, glob('%s/figures/*.png' % path)))
-        
-        omf_files = glob(omf_pattern)
-        if omf_files:
-            data_files.append((os.path.join('share', 'omf', name), omf_files))
         
         return data_files
     
